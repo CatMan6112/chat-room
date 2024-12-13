6fa318dd8fad8857f049242603dce9540634accf
neowm mofo <codymancann667@gmail.com>
2024-12-12 21:43:56 -0600

    idk what i did but it works :)

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
   // This is very IMPORTANT!! We're going to use "db" a lot.
   var db = firebase.database()
-
   const encodedKey = "c2stcHJvai1OQjM2VDI2RzJqQVJUU2Zxd2g0eDFVaDd0V180bDE4SEVUWjRGZzlMcTdadFFxbXdHeExXdWxleTFPWTA4ZDdYYmFUZkJsLUtoNVQzQmxia0ZKeGZzcHNkU0tDUkFwZ2RONlc4SnN2Rndnc3VPZE1OSXhzRnRxazBMSG9FcDN1bXhPYVFFMmpLdHJleUFFQWxBRnp3aVJyMzRJZ0E=";
   const openaiApiKey = atob(encodedKey);
   
